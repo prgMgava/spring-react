eda5cf9cbb6f3ec853147d4cc858a84b62e04434
Mgava <prgmgava@hotmail.com>
2021-11-05 11:53:31 -0300

    first request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import Chart from "react-apexcharts"
+import { SaleSum } from "types/sale"
+import { requests } from "utils/requests"
+
+interface ChartData {
+    labels: string[]
+    series: number[]
+}
 
 const DonutChart = () => {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    const [chartData, setChartData] = useState({} as ChartData)
+    // const mockData = {
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
+    requests.get("/sales/amount-by-seller").then((response)=> {
+        const data = response.data as SaleSum[]
+        const myLabels = data.map(x => x.sellerName)
+        const mySeries = data.map(x => x.sum)
+        setChartData({labels: myLabels, series: mySeries })
+    })
     
     const options = {
         legend: {
