000289d85009a4dead1b3e2f6355ac0cc776907c
Mgava <prgmgava@hotmail.com>
2021-11-05 11:25:50 -0300

    home,navigation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,28 @@
-const Home = () => {
-    return (
-        <>
-            <h1> PAGINA HOME</h1>
-        </>
-    )
-}
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="text-secondary bg-light px-5 pt-2" style={{height: "370px"}}>
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard" children="Acessar o dashboard"/>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
