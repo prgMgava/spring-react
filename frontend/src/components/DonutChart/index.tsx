675c5c5067410d3a2346ae828874b78f4e139261
Mgava <prgmgava@hotmail.com>
2021-11-05 12:00:30 -0300

    donnut chart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,37 +1,39 @@
-import { useState } from "react"
-import Chart from "react-apexcharts"
-import { SaleSum } from "types/sale"
-import { requests } from "utils/requests"
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { requests } from "utils/requests";
 
 interface ChartData {
-    labels: string[]
-    series: number[]
+  labels: string[];
+  series: number[];
 }
 
 const DonutChart = () => {
-    const [chartData, setChartData] = useState({} as ChartData)
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
-    requests.get("/sales/amount-by-seller").then((response)=> {
-        const data = response.data as SaleSum[]
-        const myLabels = data.map(x => x.sellerName)
-        const mySeries = data.map(x => x.sum)
-        setChartData({labels: myLabels, series: mySeries })
-    })
-    
-    const options = {
-        legend: {
-            show: true
-        }
-    }
-    return(
-        <Chart options={{...options, labels: mockData.labels}}
-        series={mockData.series}
-        type="donut"
-        height="240"/>
-    )
-}
+  const [chartData, setChartData] = useState({} as ChartData);
 
-export default DonutChart
+  useEffect(() => {
+    requests.get("/sales/amount-by-seller").then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+      setChartData({ labels: myLabels, series: mySeries });
+    });
+  },[]);
+ 
+
+  const options = {
+    legend: {
+      show: true,
+    },
+  };
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
+};
+
+export default DonutChart;
